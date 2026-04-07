ca7a256f8ec1439fea33e5a993db1dd81b5dac31
Garrett Plasky <garrett.plasky@gmail.com>
2026-04-06 22:51:59 -0700

    feat: add configuration schema and startup script for Home Assistant add-on

--- lcd-dashboard/run.sh
+++ lcd-dashboard/run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/with-contenv bashio
 
 # Extract user configuration from the HA Add-on UI
+export RPI_LGPIO_REVISION=$(bashio::config 'rpi_lgpio_revision')
 export SPI_BUS=$(bashio::config 'spi_bus')
 export SPI_DEVICE=$(bashio::config 'spi_device')
 export SPI_SPEED=$(bashio::config 'spi_speed')
 export GPIO_CHIP=$(bashio::config 'gpio_chip')
 export DIGITAL_BACKLIGHT=$(bashio::config 'digital_backlight')
-export VERSION="1.1.7"
+export VERSION="1.2.0"
 
 bashio::log.info "========================================="
 bashio::log.info "STARTING RPI LCD DASHBOARD"
+bashio::log.info "RPI_LGPIO_REVISION: ${RPI_LGPIO_REVISION}"
 bashio::log.info "SPI_BUS: ${SPI_BUS}"
 bashio::log.info "SPI_DEVICE: ${SPI_DEVICE}"
 bashio::log.info "SPI_SPEED: ${SPI_SPEED}"
